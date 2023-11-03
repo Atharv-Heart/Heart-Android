c87c30b058170963c74f41bc1dbd3723249b5942
vinit <vinithanabar20@gmail.com>
2023-11-04 00:13:42 +0530

    added gallery feature

--- app/src/main/java/com/accidentaldeveloper/heart/Gallery.kt
+++ app/src/main/java/com/accidentaldeveloper/heart/Gallery.kt
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.accidentaldeveloper.heart.adapters.GalleryAdapter
 import com.accidentaldeveloper.heart.viewmodel.HeartViewModel.GalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class Gallery : AppCompatActivity() {
     private val galleryViewModel:GalleryViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
-        galleryViewModel.liveData.observe(this,{
-            Log.d("vinit", "onCreate: ${it}")
+        // Find the RecyclerView in your layout
+        recyclerView = findViewById(R.id.gallery_rv)
+
+        // Set up the StaggeredGridLayoutManager with your desired span count
+        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
+        galleryViewModel.liveData.observe(this,{galleryItems->
+            val adapter = GalleryAdapter(galleryItems)
+            recyclerView.adapter = adapter
+            Log.d("vinit", "onCreate: ${galleryItems}")
         })
     }
 }
--- app/src/main/java/com/accidentaldeveloper/heart/adapters/GalleryAdapter.kt
+++ app/src/main/java/com/accidentaldeveloper/heart/adapters/GalleryAdapter.kt
@@ -1,4 +1,39 @@
 package com.accidentaldeveloper.heart.adapters
 
-class GalleryAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.accidentaldeveloper.heart.R
+import com.accidentaldeveloper.heart.models.Heart_gallery_model.gallery_responseItem
+import com.bumptech.glide.Glide
+
+
+class GalleryAdapter(private val items: List<gallery_responseItem>) :
+    RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.galleryrv, parent, false)
+        return GalleryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    inner class GalleryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: gallery_responseItem) {
+            // Use Glide to load the image from the URL into an ImageView
+            Glide.with(itemView.context)
+                .load(item.imageUrl)
+                .into(itemView.findViewById<ImageView>(R.id.vinit))
+
+
+
+        }
+    }
 }
